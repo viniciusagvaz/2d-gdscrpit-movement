bd80b3bed81d41fc7a235e2834ac33eafdd4b67f
viniciusagvaz <agvazvinicius@gmail.com>
2023-11-06 19:19:03 -0300

    walking/jumping animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,19 +7,25 @@ func _process(delta):
 	
 	if !is_on_floor():
 		velocity.y += gravity	
-		
+		$Sprite2D.frame = 1
+
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 		$Sprite2D.flip_h = false
-		$Sprite2D.frame = 3
-
+		$AnimationPlayer.play('walking')
+		
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x  = -speed
 		$Sprite2D.flip_h = true
+		$AnimationPlayer.play('walking')
+		
 	else:
 		velocity.x = 0
+		$AnimationPlayer.stop()
+		$Sprite2D.frame = 0
 	
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y -= speed
+		
 		
 	move_and_slide()
